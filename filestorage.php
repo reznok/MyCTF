dbc567107780215cc1497390f40855bf9f8d69d5
Brandon Forbes <brandonhforbes@gmail.com>
2017-09-12 16:07:35 -0700

    file storage POST -> GET

--- filestorage.php
+++ filestorage.php
@@ -11,9 +11,9 @@ function check_password($user, $pass){
     return false;
 }
 
-if (isset($_POST["username"])){
-    if (isset($_POST["pin"])) {
-        if(!check_password($_POST["username"], $_POST["pin"]))
+if (isset($_GET["username"])){
+    if (isset($_GET["pin"])) {
+        if(!check_password($_GET["username"], $_GET["pin"]))
         {
             echo "Invalid Username / PIN <br>";
         }
@@ -25,16 +25,20 @@ if (isset($_POST["username"])){
 ?>
 
 <html>
-Please enter your Username and PIN. Alternatively, you can login by going to:<br>
+Please enter your Username and PIN.<br>
+Alternatively, you can login by going to:<br>
 http://localhost/filestorage.php?username=YOUR_USERNAME&pin=YOUR_PIN<br>
 <br>
 Just replace YOUR_USERNAME with your username, and YOUR_PIN with your pin!
+<br>
+<br>
+<br>
 
 
 
 <form action="" method="GET">
     Username: <input type="text" size="40" name="username"><br>
-    PIN: <input type="text" size="4" name="pin"> <br>
+    PIN:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type="text" size="4" name="pin"> <br>
     <input type="submit" value="Submit">
 </form>
 
