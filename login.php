ad5441fece01687be8d38b5a7d571e35d5549068
Brandon Forbes <brandonhforbes@gmail.com>
2017-09-12 15:26:52 -0700

    no message

--- login.php
+++ login.php
@@ -1,6 +1,20 @@
 <?php
-    if (isset($_POST["username"]) and $_POST["username"] == "pawl"){
-        echo "Hello Pawl!";
+    function check_password($user, $pass){
+        if ($user == "pawl" && $pass == "1337hax0rz")
+            return true;
+        return false;
+    }
+
+    if (isset($_POST["username"])){
+        if (isset($_POST["password"])) {
+            if(check_password($_POST["username"], $_POST["password"]))
+            {
+                echo "You Are Now Logged In As: " . $_POST["username"];
+            }
+        }
+        else{
+            echo "Please Enter A Password";
+        }
     }
 ?>
 
