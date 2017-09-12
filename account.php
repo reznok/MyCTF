2833c48e2e600d1a18886cb2ad999ccc13ffc0f2
Brandon Forbes <brandonhforbes@gmail.com>
2017-09-12 15:55:18 -0700

    no message

--- account.php
+++ account.php
@@ -8,5 +8,13 @@ if (!isset($_SESSION["login_user"]))
     exit();
 }
 
+if ($_SESSION["login_user"] == "pawl"){
+    echo "Hello Pawl! Your Flag Is: ABCD{FLAG!}";
+}
+
 ?>
-You can't see me! 
+
+The <i>admin</i> user has implemented an online <a href="/filestorage.php">file storage</a> for everyone to use. Just login with your username and <a href="/pin.html">PIN</a>!
+
+
+
