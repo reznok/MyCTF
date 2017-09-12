34236227114a62afad07a522a8cc8cfb17eb0b60
Brandon Forbes <brandonhforbes@gmail.com>
2017-09-12 15:18:39 -0700

    no message

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
 include("header.php");
+
+?>
+
+<html>
+Welcome To My Super Secure Page! Please <a href="/login.php">Login</a>
+</html>
---
+++ login.php
@@ -0,0 +1,4 @@
+<html>
+Username: <input type="text" size="40">
+Password: <input type="password" size="40">
+</html>
