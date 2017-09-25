9254b106d68f9c0192a33fd05986dc87a936d667
Brandon Forbes <brandonhforbes@gmail.com>
2017-09-25 15:05:09 -0700

    Error reporting

--- viewfile.php
+++ viewfile.php
@@ -6,6 +6,10 @@
  * Time: 1:48 PM
  */
 
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+
 if (isset($_GET['file'])){
     $fp = fopen("zip:///var/www/MyCTF/uploads/tester.zip#readme.txt", 'rb');
     if( $fp ){
